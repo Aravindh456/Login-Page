2399cc7132e5f8d1a01526bf8a67b1d3d80828b7
Aravindh341 <126602450+Aravindh341@users.noreply.github.com>
2023-07-14 01:50:40 +0530

    forgot screen validation and Slider

--- src/forgot.js
+++ src/forgot.js
@@ -2,97 +2,111 @@ import "./login.css";
 import LoginImg from "./login-img.png";
 import Logo from "./logo.svg";
 import React from "react";
+import { useState } from "react";
+import validator from "validator";
 import LoginPage from "./login";
 import {Link} from 'react-router-dom';
-function ForgotPage() {
+const ForgotPage = () => {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const emailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const validateForm = (event) => {
+    event.preventDefault();
+
+    if (!email) {
+      setEmailError('Please enter an email address');
+    } else if (!validator.isEmail(email)) {
+      setEmailError('Please enter a valid email address');
+    } else {
+      setEmailError('');
+      console.log(email);
+      setSuccessMessage("Email Sended Successfully !");
+      setEmail("")
+      setEmailError("")
+    }
+  };
+
+   
+ 
+
   return (
-    <div class="main-container">
-      <div class="slider-container">
-        <div class="slider-content">
-          <div class="logo-img-banner">
-            <img
-              src={LoginImg}
-              alt="login-screen-image"
-              class="login-screen-image"
-            />
+    <div className="main-container">
+      <div className="slider-container">
+      <div className="slider-content">
+          <div className="slide">
+            {/* <div className="logo-img-banner">
+              <img src={LoginImg} alt="login-screen-image" className="login-screen-image" />
+            </div> */}
+            <div className="logo-img-content">
+              <h1>Welcome to Dreams LMS Courses.</h1>
+              <p className="logo-para">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi quae omnis aut quidem quos, exercitationem
+                officiis maxime! Vero, consequuntur quos. Iste, architecto quas quaerat accusamus quo facilis? Vero, dolorem
+                quo!
+              </p>
+            </div>
           </div>
-          <div class="logo-img-content">
-            <h1 style={{ color: "#002058", fontWeight: "500" }}>
-              Welcome to Dreams LMS.
-            </h1>
-            <p class="logo-para">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
-              quae omnis aut quidem quos, exercitationem officiis maxime! Vero,
-              consequuntur quos. Iste, architecto quas quaerat accusamus quo
-              facilis? Vero, dolorem quo!
-            </p>
+          <div className="slide">
+            {/* <div className="logo-img-banner">
+              <img src={LoginImg} alt="login-screen-image" className="login-screen-image" />
+            </div> */}
+            <div className="logo-img-content">
+              <h1>Welcome to Dreams LMS Courses.</h1>
+              <p className="logo-para">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi quae omnis aut quidem quos, exercitationem
+                officiis maxime! Vero, consequuntur quos. Iste, architecto quas quaerat accusamus quo facilis? Vero, dolorem
+                quo!
+              </p>
+            </div>          </div>
+          <div className="slide">
+            {/* <div className="logo-img-banner">
+              <img src={LoginImg} alt="login-screen-image" className="login-screen-image" />
+            </div> */}
+            <div className="logo-img-content">
+              <h1>Welcome to Dreams LMS Courses.</h1>
+              <p className="logo-para">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi quae omnis aut quidem quos, exercitationem
+                officiis maxime! Vero, consequuntur quos. Iste, architecto quas quaerat accusamus quo facilis? Vero, dolorem
+                quo!
+              </p>
+            </div>
           </div>
+        </div>
+        <div className="forgot-slider-dots">
+          <span className="slider-dot"></span>
+          <span className="slider-dot"></span>
+          <span className="slider-dot"></span>
         </div>
       </div>
-      {/* <div class="slider-container">
-          <div class="slider-content">
-            <div class="logo-img-banner">
-              <img
-                src={LoginImg}
-                alt="login-screen-image"
-                class="login-screen-image"
-              />
-            </div>
-            <div class="logo-img-content">
-              <h2>Welcome to Dreams LMS.</h2>
-              <p class="logo-para">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
-                quae omnis aut quidem quos, exercitationem officiis maxime! Vero,
-                consequuntur quos. Iste, architecto quas quaerat accusamus quo
-                facilis? Vero, dolorem quo!
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="slider-container">
-          <div class="slider-content">
-            <div class="logo-img-banner">
-              <img
-                src={LoginImg}
-                alt="login-screen-image"
-                class="login-screen-image"
-              />
-            </div>
-            <div class="logo-img-content">
-              <h2>Welcome to Dreams LMS.</h2>
-              <p class="logo-para">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
-                quae omnis aut quidem quos, exercitationem officiis maxime! Vero,
-                consequuntur quos. Iste, architecto quas quaerat accusamus quo
-                facilis? Vero, dolorem quo!
-              </p>
-            </div>
-          </div>
-        </div> */}
-      <div class="form-container">
-        <div class="form-logo-content">
+      <div className="form-container">
+        <div className="form-logo-content">
           <div>
-            <img src={Logo} alt="register-screen-image" class="form-logo-img" />
+            <img src={Logo} alt="register-screen-image" className="form-logo-img" />
           </div>
           <div>
             <p>
-              <a href="#" class="back-home">
+              <a href="#" className="back-home">
                 Back to Home
               </a>
             </p>
           </div>
         </div>
-        <h1 class="form-head">Forgot Password?</h1>
-        <p class="form-para">Enter your email to reset your password.</p>
-        <div class="form-content">
-          <form>
+        <h1 className="form-head">Forgot Password?</h1>
+        <p className="form-para">Enter your email to reset your password.</p>
+        <div className="form-content">
+          <form onSubmit={validateForm}>
             <label>Username or Email Address </label>
             <br />
-            <input type="text" name="Email" class="input" />
+            <input type="text" name="Email" className="input" value={email} onChange={emailChange} />
+            {emailError && <div className="error-message">{emailError}</div>}
             <br />
-            <button type="submit" class="form-btn">
+            <button type="submit" className="form-btn">
              Reset Password
             </button>
+            {successMessage && <div className="success-message">{successMessage}</div>}
           </form>
         </div>
         <div>
--- src/login.js
+++ src/login.js
@@ -3,9 +3,12 @@ import LoginImg from "./login-img.png";
 import Logo from "./logo.svg";
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
+import $ from "jquery";
 import validator from "validator";
 import ForgotPage from "./forgot";
 import { Link } from "react-router-dom";
+
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -13,12 +16,12 @@ const LoginPage = () => {
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
-  const handleEmailChange = (event) => {
+  const emailCheck = (event) => {
     setEmail(event.target.value);
     setEmailError("");
   };
 
-  const handlePasswordChange = (event) => {
+  const passwordCheck = (event) => {
     setPassword(event.target.value);
     setPasswordError("");
   };
@@ -34,6 +37,7 @@ const LoginPage = () => {
     } else if (!validator.isEmail(email)) {
       setEmailError("Please enter a valid email address");
       hasError = true;
+      console.log(email.value);
     }
 
     if (!password) {
@@ -45,86 +49,96 @@ const LoginPage = () => {
     }
 
     if (!hasError) {
-      // Perform form submission logic here
+      console.log(email);
+      console.log(password);
       console.log("Form submitted successfully");
+      setEmail("");
+      setPassword("");
+      setEmailError("");
+      setPasswordError("");
     }
   };
+
+  const disabledButton = !(email && password);
+
+
   return (
-    <div class="main-container">
-      <div class="slider-container">
-        <div class="slider-content">
-          <div class="slide">
-            <div class="logo-img-banner">
-              <img src={LoginImg} alt="login-screen-image" class="login-screen-image" />
+    <div className="main-container">
+      <div className="slider-container">
+        <div className="slider-content">
+          <div className="slide">
+            <div className="logo-img-banner">
+              <img src={LoginImg} alt="login-screen-image" className="login-screen-image" />
             </div>
-            <div class="logo-img-content">
+            <div className="logo-img-content">
               <h1>Welcome to Dreams LMS.</h1>
-              <p class="logo-para">
+              <p className="logo-para">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi quae omnis aut quidem quos, exercitationem
                 officiis maxime! Vero, consequuntur quos. Iste, architecto quas quaerat accusamus quo facilis? Vero, dolorem
                 quo!
               </p>
             </div>
           </div>
-          <div class="slide">
-          <div class="logo-img-banner">
-              <img src={LoginImg} alt="login-screen-image" class="login-screen-image" />
+          <div className="slide">
+            <div className="logo-img-banner">
+              <img src={LoginImg} alt="login-screen-image" className="login-screen-image" />
             </div>
-            <div class="logo-img-content">
+            <div className="logo-img-content">
               <h1>Welcome to Dreams LMS.</h1>
-              <p class="logo-para">
+              <p className="logo-para">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi quae omnis aut quidem quos, exercitationem
                 officiis maxime! Vero, consequuntur quos. Iste, architecto quas quaerat accusamus quo facilis? Vero, dolorem
                 quo!
               </p>
             </div>          </div>
-          <div class="slide">
-          <div class="logo-img-banner">
-              <img src={LoginImg} alt="login-screen-image" class="login-screen-image" />
+          <div className="slide">
+            <div className="logo-img-banner">
+              <img src={LoginImg} alt="login-screen-image" className="login-screen-image" />
             </div>
-            <div class="logo-img-content">
+            <div className="logo-img-content">
               <h1>Welcome to Dreams LMS.</h1>
-              <p class="logo-para">
+              <p className="logo-para">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi quae omnis aut quidem quos, exercitationem
                 officiis maxime! Vero, consequuntur quos. Iste, architecto quas quaerat accusamus quo facilis? Vero, dolorem
                 quo!
               </p>
-            </div>          </div>
+            </div>
+          </div>
         </div>
-        <div class="slider-dots">
-          <span class="slider-dot"></span>
-          <span class="slider-dot"></span>
-          <span class="slider-dot"></span>
+        <div className="slider-dots">
+          <span className="slider-dot"></span>
+          <span className="slider-dot"></span>
+          <span className="slider-dot"></span>
         </div>
       </div>
 
-      <div class="form-container">
-        <div class="form-logo-content">
+      <div className="form-container">
+        <div className="form-logo-content">
           <div>
-            <img src={Logo} alt="register-screen-image" class="form-logo-img" />
+            <img src={Logo} alt="register-screen-image" className="form-logo-img" />
           </div>
           <div>
             <p>
-              <a href="#" class="back-home">
+              <a href="#" className="back-home">
                 Back to Home
               </a>
             </p>
           </div>
         </div>
-        <h3 class="form-head">Login to you account</h3>
-        <div class="form-content">
+        <h3 className="form-head">Login to you account</h3>
+        <div className="form-content">
           <form onSubmit={validateForm}>
             <label>Username or Email Address </label>
             <br />
             <input
               type="text"
               name="Email"
-              class="input"
+              className="input"
               value={email}
-              onChange={handleEmailChange}
+              onChange={emailCheck}
             />
             {emailError && (
-              <div className="error-message" style={{ color: "red" }}>
+              <div className="error-message" >
                 {emailError}
               </div>
             )}
@@ -135,21 +149,21 @@ const LoginPage = () => {
             <input
               type="password"
               name="password"
-              class="input"
+              className="input"
               value={password}
-              onChange={handlePasswordChange}
+              onChange={passwordCheck}
             />
             {passwordError && (
-              <div className="error-message" style={{ color: "red" }}>
+              <div className="error-message">
                 {passwordError}
               </div>
             )}
             <br />
 
-            <div class="check">
-              <div class="check-box">
-                <input type="checkbox" id="remember" class="checkbox" />
-                <label for="remember">Remember me</label>
+            <div className="check">
+              <div className="check-box">
+                <input type="checkbox" id="remember" className="checkbox" />
+                <label htmlFor="remember">Remember me</label>
               </div>
               <div>
                 <Link className="link" to="/forgot">
@@ -158,7 +172,7 @@ const LoginPage = () => {
               </div>
             </div>
             <br />
-            <button type="submit" class="form-btn">
+            <button type="submit" className={`form-btn ${disabledButton ? 'disabled' : ''}`} disabled={disabledButton}>
               Login
             </button>
           </form>
